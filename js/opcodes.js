5079a28cea1f9491b8ad03794324cf7ee172e1d3
Gil Mendes <gil00mendes@gmail.com>
2023-03-22 09:51:55 +0000

    Extract opcodes into a separated structure
    With this structure it will be possible to share logic between the compiler and emulator.
    
    Also, the code becomes simpler since we work with the opcode name instead of the hex code.
    
    As future work, we can do the same with the compiler.

---
+++ js/opcodes.js
@@ -0,0 +1,21 @@
+/**
+ * List of available opcodes.
+ */
+export const opcodes = {
+    NOP: 0x0,
+    JNE: 0x1,
+    JL: 0x2,
+    JM: 0x3,
+    JE: 0x4,
+    FLAGS: 0x5,
+    JUMP: 0x6,
+    RB2A: 0x7,
+    RA2A: 0x8,
+    A2RB: 0x9,
+    A2RA: 0xa,
+    CALL: 0xb,
+    RETURN: 0xc,
+    SYSCALL: 0xd,
+    DEBUG: 0xe,
+    EXIT: 0xf,
+};
